ce3a702c215f3d17129fe7daed3e5f52109f7480
Selwin van Dijk <basjetimmer@yahoo.com>
2019-09-13 09:06:08 +0200

    Mostly just some missing licenses

--- arg/arg.cc
+++ arg/arg.cc
@@ -1,3 +1,22 @@
+/*
+    Copyright (C)   Selwin van Dijk
+
+    This file is part of .
+
+     is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+     is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with .  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "arg.h"
 
 Arg::Arg(int argc, char *argv[])
--- signalbackup/exportbackup.cc
+++ signalbackup/exportbackup.cc
@@ -1,3 +1,22 @@
+/*
+    Copyright (C)   Selwin van Dijk
+
+    This file is part of .
+
+     is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+     is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with .  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "signalbackup.ih"
 
 void SignalBackup::exportBackup(std::string const &directory)
@@ -46,14 +65,12 @@ void SignalBackup::exportBackup(std::string const &directory)
 
   // export sharedpreferences
   std::cout << "Writing SharedPrefFrame(s)..." << std::endl;
-  int count = 0;
-  for (auto const &spframe : d_sharedpreferenceframes)
+  for (int count = 0; auto const &spframe : d_sharedpreferenceframes)
     writeRawFrameDataToFile(directory + "/SharedPreference_" + bepaald::toString(count++) + ".sbf", spframe);
 
   // export stickers
   std::cout << "Writing StickerFrames..." << std::endl;
-  count = 0;
-  for (auto const &sframe : d_stickers)
+  for (int count = 0; auto const &sframe : d_stickers)
   {
     StickerFrame *s = sframe.second.get();
     writeRawFrameDataToFile(directory + "/Sticker_" + bepaald::toString(count++) + ".sbf", s);
