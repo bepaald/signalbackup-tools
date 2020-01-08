456cf3dc522e9b3d0619993cdb474c5f8a22dcfd
Selwin van Dijk <basjetimmer@yahoo.com>
2020-01-08 09:53:15 +0100

    first small fixes for exportxml (support old dbs)

--- autoversion.h
+++ autoversion.h
@@ -1,6 +1,25 @@
+/*
+    Copyright (C) 2020  Selwin van Dijk
+
+    This file is part of signalbackup-tools.
+
+    signalbackup-tools is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    signalbackup-tools is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with signalbackup-tools.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSIONDATE "20200105.125856"
+#define VERSIONDATE "20200108.095303"
 
 #endif
