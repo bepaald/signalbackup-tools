98f83b5c7077a934ac0db91d09d9d1ac94223f82
Selwin van Dijk <basjetimmer@yahoo.com>
2021-01-21 18:16:27 +0100

    A bit more work on wa-chat import.

--- autoversion.h
+++ autoversion.h
@@ -20,6 +20,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSIONDATE "20210119.184346"
+#define VERSIONDATE "20210121.181624"
 
 #endif
--- signalbackup/importwachat.cc
+++ signalbackup/importwachat.cc
@@ -25,8 +25,9 @@
   Limitations and requirements
 
   - no media (yet?)
-  - filename must exactly and uniquely identify an existing conversation in the Signal backup
-  - messages have format '<timestamp> <author>: <message>' where timestamp is passed as 'fmt', author exactly
+  - no mentions YET
+  - filename must exactly and uniquely identify an existing conversation (group or contact name) in the Signal backup
+  - messages have format '<timestamp><author>: <message>' where timestamp is passed as 'fmt', author exactly
     and uniquely identifies an existing contact in the Signal backup and contains no colons (':')
   -
 
@@ -41,7 +42,7 @@ bool handleWAMessage(long long int time, std::string const &chatname, std::strin
 }
 */
 
-bool SignalBackup::importWAChat(std::string const &file, std::string const &fmt)
+bool SignalBackup::importWAChat(std::string const &file, std::string const &fmt, std::string const &self)
 {
   std::ifstream chatfile(file);
   if (!chatfile.is_open())
@@ -54,11 +55,52 @@ bool SignalBackup::importWAChat(std::string const &file, std::string const &fmt)
 
   // get global address (recipient_id of chat partner for 1-on-1, group_id for groupchats
   std::string globaladdress;
-  //bool isgroup;
+  bool isgroup = false;
+  SqliteDB::QueryResults results;
+
+  std::cout << "Looking for conversation: '" << file.substr(0, file.length() - STRLEN(".txt")) << "'" << std::endl;
+
+  if (!d_database.exec("SELECT recipient._id, recipient.group_id, COALESCE(groups.title, recipient.system_display_name, recipient.profile_joined_name, recipient.phone) AS 'identifier' "
+                       "FROM recipient "
+                       "LEFT JOIN groups ON recipient.group_id == groups.group_id "
+                       "WHERE identifier == ?", file.substr(0, file.length() - STRLEN(".txt")), &results))
+    return false;
+
+  //results.prettyPrint();
+
+  if (results.rows() == 1)
+  {
+    globaladdress = results.valueAsString(0, "_id");
+    if (results.valueHasType<std::string>(0, "group_id"))
+    {
+      isgroup = true;
+      if (self.empty())
+      {
+        std::cout << "Error: dealing with group chat, but self-id not supplied. No way of determining which messages are outgoing and which are incoming." << std::endl;
+        return false;
+      }
+    }
+  }
+  else
+  {
+    std::cout << "Failed to find conversation partner/group for chat: '" << file << "' : query returned " << results.rows() << " results." << std::endl;
+    return false;
+  }
 
   // get thread id from recipient
-  //long long int tid;
+  long long int tid = -1;
+  if (!d_database.exec("SELECT _id FROM thread WHERE recipient_ids == ?", globaladdress, &results))
+    return false;
 
+  if (results.rows() != 1)
+  {
+    std::cout << "Failed to find thread for chat. Query returned " << results.rows() << " results." << std::endl;
+    return false;
+  }
+
+  tid = results.getValueAs<long long int>(0, "_id");
+
+  std::cout << "Importing messages into thread: " << tid << std::endl;
 
   std::string line;
   long long int time = 0;
@@ -94,9 +136,9 @@ bool SignalBackup::importWAChat(std::string const &file, std::string const &fmt)
       {
         // deal with message
         std::cout << "Dealing with message:" << std::endl;
-        std::cout << "Time: " << time << std::endl;
-        std::cout << "Author: " << author << std::endl;
-        std::cout << "Message: " << message << std::endl;
+        std::cout << "Time: '" << time << "'" << std::endl;
+        std::cout << "Author: '" << author << "'" << std::endl;
+        std::cout << "Message: '" << message << "'" << std::endl;
       }
 
       // get a unique sequentially later time
@@ -106,7 +148,7 @@ bool SignalBackup::importWAChat(std::string const &file, std::string const &fmt)
       else
         previous_time = time;
       previous_time_adj = time;
-      std::cout << "Time: " << std::put_time(&tmb, "%c") << " (" << time << ")" << std::endl;
+      //std::cout << "Time: " << std::put_time(&tmb, "%c") << " (" << time << ")" << std::endl;
 
       std::getline(ss, author, ':');
       if (ss.eof())
@@ -114,10 +156,12 @@ bool SignalBackup::importWAChat(std::string const &file, std::string const &fmt)
         std::cout << "No colon => some type of status message?" << std::endl;
         continue;
       }
-      std::cout << "Author: " << author << std::endl;
+      //std::cout << "Author: " << author << std::endl;
 
       std::getline(ss, message);
-      std::cout << "Message: " << message << std::endl;
+      message = message.substr(1); // remove the single whitespace after colon??
+
+      //std::cout << "Message: " << message << std::endl;
     }
   }
 
@@ -125,9 +169,9 @@ bool SignalBackup::importWAChat(std::string const &file, std::string const &fmt)
   if (time != 0)
   {
     std::cout << "Dealing with message:" << std::endl;
-    std::cout << "Time: " << time << std::endl;
-    std::cout << "Author: " << author << std::endl;
-    std::cout << "Message: " << message << std::endl;
+    std::cout << "Time: '" << time << "'" << std::endl;
+    std::cout << "Author: '" << author << "'" << std::endl;
+    std::cout << "Message: '" << message << "'" << std::endl;
   }
 
   return false;
