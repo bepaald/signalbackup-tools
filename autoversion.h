bepaald/signalbackup-tools

commit 7c5ae9ed874368f2ff2daf07fd6cdb841571bf2d

Trying to blindly deal with remapped_recipients, good chance its buggy

signalbackup/cleandatabasebymessages.cc
@@ -52,14 +52,19 @@ void SignalBackup::cleanDatabaseByMessages()
     // this gets all recipient_ids/addresses ('+31612345678') from still existing groups and sms/mms
     d_database.exec("DELETE FROM recipient_preferences WHERE recipient_ids NOT IN (WITH RECURSIVE split(word, str) AS (SELECT '', members||',' FROM groups UNION ALL SELECT substr(str, 0, instr(str, ',')), substr(str, instr(str, ',')+1) FROM split WHERE str!='') SELECT DISTINCT split.word FROM split WHERE word!='' UNION SELECT DISTINCT address FROM sms UNION SELECT DISTINCT address FROM mms)");
   }
-  else // untested
+  else
   {
     std::cout << "  Deleting unreferenced recipient entries..." << std::endl;
+
     //runSimpleQuery("SELECT group_concat(_id,',') FROM recipient");
     //runSimpleQuery("WITH RECURSIVE split(word, str) AS (SELECT '', members||',' FROM groups UNION ALL SELECT substr(str, 0, instr(str, ',')), substr(str, instr(str, ',')+1) FROM split WHERE str!='') SELECT DISTINCT split.word FROM split WHERE word!='' UNION SELECT DISTINCT address FROM sms UNION SELECT DISTINCT address FROM mms UNION SELECT DISTINCT recipient_ids FROM thread");
 
     // this gets all recipient_ids/addresses ('+31612345678') from still existing groups and sms/mms
+
+    // KEEP recipients WITH _id IN remapped_recipients.old_id!?!?
+
     d_database.exec("DELETE FROM recipient WHERE _id NOT IN (WITH RECURSIVE split(word, str) AS (SELECT '', members||',' FROM groups UNION ALL SELECT substr(str, 0, instr(str, ',')), substr(str, instr(str, ',')+1) FROM split WHERE str!='') SELECT DISTINCT split.word FROM split WHERE word!='' UNION SELECT DISTINCT address FROM sms UNION SELECT DISTINCT address FROM mms UNION SELECT DISTINCT recipient_ids FROM thread)");
+
   }
 
   //runSimpleQuery((d_databaseversion < 27) ? "SELECT _id, recipient_ids, system_display_name FROM recipient_preferences" : "SELECT _id, COALESCE(system_display_name,group_id,signal_profile_name) FROM recipient");
@@ -122,12 +127,22 @@ void SignalBackup::cleanDatabaseByMessages()
   else
     d_database.exec("DELETE FROM identities WHERE address NOT IN (SELECT DISTINCT _id FROM recipient)");
 
-
   std::cout << "  Deleting group receipts entries from deleted messages..." << std::endl;
   d_database.exec("DELETE FROM group_receipts WHERE mms_id NOT IN (SELECT DISTINCT _id FROM mms)");
 
   std::cout << "  Deleting drafts from deleted threads..." << std::endl;
   d_database.exec("DELETE FROM drafts WHERE thread_id NOT IN (SELECT DISTINCT thread_id FROM sms) AND thread_id NOT IN (SELECT DISTINCT thread_id FROM mms)");
+
+  if (d_database.containsTable("remapped_recipients"))
+  {
+    std::cout << "  Deleting remapped recipients for non existing recipients" << std::endl;
+    //d_database.exec("DELETE FROM remapped_recipients WHERE old_id NOT IN (SELECT DISTINCT _id FROM recipient_preferences) AND new_id NOT IN (SELECT DISTINCT _id FROM recipient_preferences)");
+    d_database.exec("DELETE FROM remapped_recipients WHERE new_id NOT IN (SELECT DISTINCT _id FROM recipient_preferences)");
+  }
+
+
+  // maybe remap recipients?
+
 
   //runSimpleQuery("SELECT _id, recipient_ids, system_display_name FROM recipient_preferences");
 

signalbackup/importthread.cc
@@ -32,6 +32,39 @@ void SignalBackup::importThread(SignalBackup *source, long long int thread)
     return;
   }
 
+  if (source->d_database.containsTable("remapped_recipients"))
+  {
+    SqliteDB::QueryResults r;
+    source->d_database.exec("SELECT * FROM remapped_recipients", &r);
+    if (r.rows())
+    {
+      std::cout << "WARNING: Source database contains 'remapped_recipients'. This case may not yet be handled correctly by this program!" << std::endl;
+
+      for (uint i = 0; i < r.rows(); ++i)
+      {
+        long long int id = r.getValueAs<long long int>(i, "_id");
+        long long int oldid = r.getValueAs<long long int>(i, "old_id");
+        long long int newid = r.getValueAs<long long int>(i, "new_id");
+        std::cout << id << " : " << oldid << " -> " << newid << std::endl;
+        std::cout << std::endl;
+
+        std::cout << "Old id:" << std::endl;
+        source->d_database.print("SELECT * FROM recipient WHERE _id = ?", oldid);
+        std::cout << std::endl;
+
+        std::cout << "New id: " << std::endl;
+        source->d_database.print("SELECT * FROM recipient WHERE _id = ?", newid);
+        std::cout << std::endl;
+
+      }
+
+      // apply the remapping (probably only some reactions _may_ need to be tranferred?)
+      source->remapRecipients();
+      // now, the remapping was 'applied', old_id should not occur in database anymore, and remapped_recipients can be cleared?
+      source->d_database.exec("DELETE FROM remapped_recipients");
+    }
+  }
+
   // crop the source db to the specified thread
   source->cropToThread(thread);
 
@@ -110,7 +143,44 @@ void SignalBackup::importThread(SignalBackup *source, long long int thread)
   if (d_database.containsTable("megaphone"))
     offsetmegaphone = getMaxUsedId("megaphone") + 1 - source->getMinUsedId("megaphone");
 
-  source->makeIdsUnique(offsetthread, offsetsms, offsetmms, offsetpart, offsetrecipient, offsetgroups, offsetidentities, offsetgroup_receipts, offsetdrafts, offsetsticker, offsetmegaphone);
+  long long int offsetremapped_recipients = -1;
+  if (d_database.containsTable("remapped_recipients"))
+    offsetremapped_recipients = getMaxUsedId("remapped_recipients") + 1 - source->getMinUsedId("remapped_recipients");
+
+  long long int offsetremapped_threads = -1;
+  if (d_database.containsTable("remapped_threads"))
+    offsetremapped_threads = getMaxUsedId("remapped_threads") + 1 - source->getMinUsedId("remapped_threads");
+
+  long long int offsetmention = -1;
+  if (d_database.containsTable("mention"))
+    offsetmention = getMaxUsedId("mention") + 1 - source->getMinUsedId("mention");
+
+  source->makeIdsUnique(offsetthread, offsetsms, offsetmms,
+                        offsetpart, offsetrecipient, offsetgroups,
+                        offsetidentities, offsetgroup_receipts, offsetdrafts,
+                        offsetsticker, offsetmegaphone, offsetremapped_recipients,
+                        offsetremapped_threads, offsetmention);
+
+  // delete double remapped_recipients
+  if (d_database.containsTable("remapped_recipients") && source->d_database.containsTable("remapped_recipients"))
+  {
+    SqliteDB::QueryResults res;
+    source->d_database.exec("SELECT * FROM remapped_recipients", &res); // get all remapped recipients in source
+
+    for (uint i = 0; i < res.rows(); ++i)
+    {
+      long long int id = res.getValueAs<long long int>(i, "_id");
+      long long int oldid = res.getValueAs<long long int>(i, "old_id");
+      long long int newid = res.getValueAs<long long int>(i, "new_id");
+      SqliteDB::QueryResults r2;
+      d_database.exec("SELECT * FROM remapped_recipients WHERE old_id = ? AND new_id = ?", {oldid, newid}, &r2);
+      if (r2.rows()) // this mapping is in target already
+      {
+        std::cout << "Skipping import of remapped_recipient (" << oldid << " -> " << newid << "), mapping alrady in target database" << std::endl;
+        source->d_database.exec("DELETE FROM remapped_recipients WHERE _id = ?", id);
+      }
+    }
+  }
 
   // merge into existing thread, set the id on the sms, mms, and drafts
   // drop the recipient_preferences, identities and thread tables, they are already in the target db

signalbackup/signalbackup.h
@@ -118,12 +118,15 @@ class SignalBackup
   template <typename T> inline std::pair<unsigned char*, size_t> numToData(T num) const;
   void setMinimumId(std::string const &table, long long int offset, std::string const &col = "_id") const;
   void cleanDatabaseByMessages();
+  void remapRecipients();
   void compactIds(std::string const &table, std::string const &col = "_id");
-  void makeIdsUnique(long long int thread, long long int sms, long long int mms, long long int part,
-                     long long int recipient_preferences, long long int groups, long long int identies,
-                     long long int group_receipts, long long int drafts, long long int sticker,
-                     long long int megaphone);
+  void makeIdsUnique(long long int minthread, long long int minsms, long long int minmms,
+                     long long int minpart, long long int minrecipient, long long int mingroups,
+                     long long int minidentities, long long int mingroup_receipts, long long int mindrafts,
+                     long long int minsticker, long long int minmegaphone, long long int minremapped_recipients,
+                     long long int minremapped_threads, long long int minmention);
   void updateRecipientId(long long int targetid, std::string ident);
+  void updateRecipientId(long long int targetid, long long int sourceid, bool verbose = false);
   long long int dateToMSecsSinceEpoch(std::string const &date, bool *fromdatestring = nullptr) const;
   long long int getThreadIdFromRecipient(std::string const &recipient) const;
   void dumpInfoOnBadFrame(std::unique_ptr<BackupFrame> *frame);

commit 4489f6f748ff8efeda10c35bc5cff49a9a2034a0

Fix merge bug introduced in 7c5ae9ed874368f2ff2daf07fd6cdb841571bf2d and nasty memory leak introduced in e220884dc68894439c764d628418715cd32323dd

autoversion.h
@@ -20,6 +20,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSIONDATE "20210211.170548"
+#define VERSIONDATE "20210214.170029"
 
 #endif

backupframe/backupframe.h
@@ -133,6 +133,7 @@ inline BackupFrame::BackupFrame(unsigned char *data, size_t l, uint64_t num)
   d_count(num),
   d_constructedsize(l)
 {
+  //std::cout << "CREATING BACKUPFRAME!" << std::endl;
   DEBUGOUT("CREATING BACKUPFRAME!");
   DEBUGOUT("DATA: ", bepaald::bytesToHexString(data, l), " (", l, " bytes)");
   d_ok = init(data, l, &d_framedata);
@@ -207,6 +208,7 @@ inline BackupFrame &BackupFrame::operator=(BackupFrame const &other)
 
 inline BackupFrame::~BackupFrame()
 {
+  //std::cout << "DESTROYING BACKUPFRAME!" << std::endl;
   for (uint i = 0; i < d_framedata.size(); ++i)
     if (std::get<1>(d_framedata[i]))
       delete[] std::get<1>(d_framedata[i]);
@@ -324,7 +326,7 @@ inline BackupFrame *BackupFrame::instantiate(FRAMETYPE ft, unsigned char *data, 
     return nullptr;
   }
 
-  return (it->second)(data, length, count);
+  return ret;
 }
 
 // maybe check endianness?

basedecryptor/getattachment.cc
@@ -23,6 +23,9 @@
 
 int BaseDecryptor::getAttachment(FrameWithAttachment *frame) // static
 {
+  //std::cout << " *** REALLY GETTING ATTACHMENT ***" << std::endl;
+  //std::cout << "          length : " << frame->length() << std::endl;
+
   std::ifstream file(frame->filename(), std::ios_base::binary | std::ios_base::in);
   if (!file.is_open())
   {

common_be.h
@@ -316,4 +316,44 @@ inline std::ostream &bepaald::bold_off(std::ostream &os)
   return os << "\033[0m";
 }
 
+
+#ifdef SIGNALBACKUP_TOOLS_REPORT_MEM
+#define MEMINFO(...) process_mem_usage(__VA_ARGS__)
+
+#include <unistd.h>
+#include <fstream>
+#include <string>
+
+// code adapted from https://stackoverflow.com/questions/669438 by Don Wakefield
+template<typename ...Args>
+void process_mem_usage(Args && ...args)
+{
+  std::cout.copyfmt(std::ios(nullptr));
+  (std::cout << ... << args) << std::endl;
+
+  // the two fields we want
+  unsigned long vsize = 0;
+  long rss = 0;
+  {
+  // dummy vars for leading entries in stat that we don't care about
+  std::string dummy;
+  // 'file' stat seems to give the most reliable results
+  std::ifstream stat_stream("/proc/self/stat", std::ios_base::in);
+  stat_stream >> dummy >> dummy >> dummy >> dummy >> dummy >> dummy
+              >> dummy >> dummy >> dummy >> dummy >> dummy >> dummy
+              >> dummy >> dummy >> dummy >> dummy >> dummy >> dummy
+              >> dummy >> dummy >> dummy >> dummy >> vsize >> rss; // don't care about the rest
+  }
+
+  long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+  double vm_usage     = vsize / 1024.0;
+  double resident_set = rss * page_size_kb;
+
+  std::cout << "VM: " << std::fixed << std::setprecision(2) << (vm_usage / 1024) << "MB ; RSS: " << (resident_set / 1024) << "MB" << std::endl;
+}
+#else
+#define MEMINFO(...)
+#endif
+
+
 #endif

databaseversionframe/databaseversionframe.h
@@ -56,7 +56,9 @@ inline DatabaseVersionFrame::DatabaseVersionFrame(uint64_t count)
 inline DatabaseVersionFrame::DatabaseVersionFrame(unsigned char *bytes, size_t length, uint64_t count)
   :
   BackupFrame(bytes, length, count)
-{}
+{
+  //std::cout << "CREATING DATABASEVERSIONFRAME" << std::endl;
+}
 
 inline BackupFrame *DatabaseVersionFrame::create(unsigned char *bytes, size_t length, uint64_t count) // static
 {

main.cc
@@ -75,6 +75,8 @@ int main(int argc, char *argv[])
     return 1;
   }
 
+  MEMINFO("Start of program, before opening input");
+
   // open input
   std::unique_ptr<SignalBackup> sb(new SignalBackup(arg.input(), arg.password(), arg.verbose(),
                                                     (!arg.source().empty() || arg.listthreads() ||
@@ -90,6 +92,8 @@ int main(int argc, char *argv[])
     std::cout << "Failed to open backup" << std::endl;
     return 1;
   }
+
+  MEMINFO("Input opened");
 
   if (arg.listthreads())
     sb->listThreads();
@@ -111,8 +115,13 @@ int main(int argc, char *argv[])
     std::vector<int> threads = arg.importthreads();
     if (threads.size() == 1 && threads[0] == -1) // import all threads!
     {
+
+      MEMINFO("Before first time reading source");
+
       std::cout << "Requested ALL threads, reading source to get thread list" << std::endl;
       source.reset(new SignalBackup(arg.source(), arg.sourcepassword(), arg.verbose(), SignalBackup::LOWMEM, arg.showprogress()));
+
+      MEMINFO("After first time reading source");
 
       source->summarize();
       sourcesummarized = true;
@@ -126,6 +135,9 @@ int main(int argc, char *argv[])
 
     for (uint i = 0; i < threads.size(); ++i)
     {
+
+      MEMINFO("Before reading source: ", i + 1, "/", threads.size());
+
       std::cout << std::endl << "Importing thread " << threads[i] << " (" << i + 1 << "/" << threads.size() << ") from source file: " << arg.source() << std::endl;
       source.reset(new SignalBackup(arg.source(), arg.sourcepassword(), arg.verbose(), SignalBackup::LOWMEM, arg.showprogress()));
       if (!sourcesummarized)
@@ -133,7 +145,9 @@ int main(int argc, char *argv[])
         source->summarize();
         sourcesummarized = true;
       }
+      MEMINFO("After reading source: ", i + 1, "/", threads.size(), " before import");
       sb->importThread(source.get(), threads[i]);
+      MEMINFO("After import");
     }
   }
 
@@ -151,7 +165,7 @@ int main(int argc, char *argv[])
     for (uint i = 0; i < arg.croptodates().size(); i += 2)
       dates.push_back({arg.croptodates()[i], arg.croptodates()[i + 1]});
     sb->cropToDates(dates);
-    //sb->cropToDates({{"2019-09-18 00:00:00", "2020-09-18 00:00:00"}});
+    // e.g.: sb->cropToDates({{"2019-09-18 00:00:00", "2020-09-18 00:00:00"}});
   }
 
   if (!arg.croptothreads().empty())
@@ -198,6 +212,8 @@ int main(int argc, char *argv[])
     sb->hhenkel(arg.hhenkel());
   }
 
+  MEMINFO("Before output");
+
   // export output
   if (!arg.output().empty())
     if (!sb->exportBackup(arg.output(), arg.opassword(), arg.overwrite(), SignalBackup::DROPATTACHMENTDATA))
@@ -206,22 +222,7 @@ int main(int argc, char *argv[])
       return 1;
     }
 
-  //sb->cropToThread({8, 10, 11});
-  //sb->listThreads();
-
-  // std::cout << "Starting export!" << std::endl;
-  // sb.exportBackup("NEWFILE");
-  // std::cout << "Finished" << std::endl;
-
-  // // std::cout << "Starting export!" << std::endl;
-  // // sb2.exportBackup("NEWFILE2");
-  // // std::cout << "Finished" << std::endl;
-
-  //auto itm = arg.importthreadsmanual();
-  //for (uint i = 0; i < itm.size(); ++i)
-  //{
-  //  std::cout << itm[i].first << "  =  " << itm[i].second  << std::endl;
-  //}
+  MEMINFO("After output");
 
   // decode and dump Signal-Desktop database to 'desktop.db'.
   if (!arg.dumpdesktopdb().empty())
@@ -236,6 +237,8 @@ int main(int argc, char *argv[])
 #if defined(_WIN32) || defined(__MINGW64__)
   SetConsoleOutputCP(oldcodepage);
 #endif
+
+  MEMINFO("At program end");
 
   return 0;
 }

signalbackup/cleandatabasebymessages.cc
@@ -136,13 +136,14 @@ void SignalBackup::cleanDatabaseByMessages()
   if (d_database.containsTable("remapped_recipients"))
   {
     std::cout << "  Deleting remapped recipients for non existing recipients" << std::endl;
-    //d_database.exec("DELETE FROM remapped_recipients WHERE old_id NOT IN (SELECT DISTINCT _id FROM recipient_preferences) AND new_id NOT IN (SELECT DISTINCT _id FROM recipient_preferences)");
-    d_database.exec("DELETE FROM remapped_recipients WHERE new_id NOT IN (SELECT DISTINCT _id FROM recipient_preferences)");
+    //d_database.exec("DELETE FROM remapped_recipients WHERE old_id NOT IN (SELECT DISTINCT _id FROM recipient) AND new_id NOT IN (SELECT DISTINCT _id FROM recipient)");
+    d_database.exec("DELETE FROM remapped_recipients WHERE new_id NOT IN (SELECT DISTINCT _id FROM recipient)");
   }
 
-
+  std::cout << "  Vacuuming database" << std::endl;
+  d_database.exec("VACUUM");
+  d_database.freeMemory();
   // maybe remap recipients?
-
 
   //runSimpleQuery("SELECT _id, recipient_ids, system_display_name FROM recipient_preferences");
 

signalbackup/exporttodir.cc
@@ -19,7 +19,7 @@
 
 #include "signalbackup.ih"
 
-bool SignalBackup::exportBackupToDir(std::string const &directory, bool overwrite)
+bool SignalBackup::exportBackupToDir(std::string const &directory, bool overwrite, bool keepattachmentdatainmemory)
 {
   std::cout << std::endl << "Exporting backup into '" << directory << "/'" << std::endl;
 
@@ -70,6 +70,9 @@ bool SignalBackup::exportBackupToDir(std::string const &directory, bool overwrit
     else
       if (!attachmentstream.write(reinterpret_cast<char *>(a->attachmentData()), a->attachmentSize()))
         return false;
+
+    if (!keepattachmentdatainmemory)
+      a->clearData();
   }
 
   // export avatars

signalbackup/exporttofile.cc
@@ -82,6 +82,7 @@ bool SignalBackup::exportBackupToFile(std::string const &filename, std::string c
   SqliteDB::QueryResults results;
   d_database.exec(q, &results);
   std::vector<std::string> tables;
+
   for (uint i = 0; i < results.rows(); ++i)
   {
     if (!results.valueHasType<std::nullptr_t>(i, 0))
@@ -91,8 +92,8 @@ bool SignalBackup::exportBackupToFile(std::string const &filename, std::string c
            STRING_STARTS_WITH(results.getValueAs<std::string>(i, 1), "sms_fts")))
         ;//std::cout << "Skipping " << results[i][1].second << " because it is smsftssecrettable" << std::endl;
       else if (results.valueHasType<std::string>(i, 1) &&
-               (results.getValueAs<std::string>(i, 1) != "mms_fts" &&
-                STRING_STARTS_WITH(results.getValueAs<std::string>(i, 1), "mms_fts")))
+                (results.getValueAs<std::string>(i, 1) != "mms_fts" &&
+                 STRING_STARTS_WITH(results.getValueAs<std::string>(i, 1), "mms_fts")))
         ;//std::cout << "Skipping " << results[i][1].second << " because it is smsftssecrettable" << std::endl;
       else
       {
@@ -164,7 +165,11 @@ bool SignalBackup::exportBackupToFile(std::string const &filename, std::string c
           if (!writeEncryptedFrame(outputfile, attachment->second.get()))
             return false;
           if (!keepattachmentdatainmemory)
+          {
+            MEMINFO("BEFORE DROPPING ATTACHMENT DATA");
             attachment->second.get()->clearData();
+            MEMINFO("AFTER DROPPING ATTACHMENT DATA");
+          }
         }
         else
         {

signalbackup/importthread.cc
@@ -373,4 +373,7 @@ void SignalBackup::importThread(SignalBackup *source, long long int thread)
   // update thread snippet and date and count
   updateThreadsEntries();
 
+  d_database.exec("VACUUM");
+  d_database.freeMemory();
+
 }

signalbackup/initfromfile.cc
@@ -47,10 +47,11 @@ void SignalBackup::initFromFile()
                 << "... " << std::flush;
       [[unlikely]] if (d_verbose)
         std::cout << "\n";
-
     }
 
     //std::cout << frame->frameTypeString() << std::endl;
+
+    //MEMINFO("At frame ", frame->frameNumber(), " (", frame->frameTypeString(), ")");
 
     [[unlikely]] if (frame->frameType() == BackupFrame::FRAMETYPE::HEADER)
     {
@@ -68,6 +69,7 @@ void SignalBackup::initFromFile()
     else [[likely]] if (frame->frameType() == BackupFrame::FRAMETYPE::SQLSTATEMENT)
     {
       SqlStatementFrame *s = reinterpret_cast<SqlStatementFrame *>(frame.get());
+
       [[unlikely]] if (s->statement().find("CREATE TABLE sqlite_") == std::string::npos) // skip creation of sqlite_ internal db's
       {
         if (!d_database.exec(s->bindStatement(), s->parameters()))
@@ -78,7 +80,6 @@ void SignalBackup::initFromFile()
       {
         // force early creation of sqlite_sequence table, this is completely unnessecary and only used
         // to get byte-identical backups during testing
-        std::cout << "Forcing early creation of sqlite_sequence table..." << std::endl;
         d_database.exec("CREATE TABLE dummy (_id INTEGER PRIMARY KEY AUTOINCREMENT)");
         d_database.exec("DROP TABLE dummy");
       }

signalbackup/removedoubles.cc
@@ -27,7 +27,7 @@ void SignalBackup::removeDoubles()
   d_database.exec("DELETE FROM sms WHERE _id IN (SELECT _id FROM (SELECT ROW_NUMBER() OVER () RNum,* FROM (SELECT DISTINCT t1.* FROM sms AS t1 INNER JOIN sms AS t2 ON t1.date = t2.date AND (t1.body = t2.body OR (t1.body IS NULL AND t2.body IS NULL)) AND t1.thread_id = t2.thread_id AND t1.address = t2.address AND t1.read = t2.read AND t1.type = t2.type AND (t1.protocol = t2.protocol OR (t1.protocol IS NULL AND t2.protocol IS NULL)) AND t1.date_sent = t2.date_sent AND t1._id <> t2._id) AS doubles ORDER BY date ASC, date_sent ASC, body ASC, thread_id ASC, address ASC, read ASC, type ASC, protocol ASC,_id ASC) t WHERE RNum%2 = 0)");
   std::cout << "  Removed " << d_database.changed() << " entries." << std::endl;
 
-  std::cout << "  Removing duplicate entries from sms table..." << std::endl;
+  std::cout << "  Removing duplicate entries from mms table..." << std::endl;
   d_database.exec("DELETE FROM mms WHERE _id IN (SELECT _id FROM (SELECT ROW_NUMBER() OVER () RNum,* FROM (SELECT DISTINCT t1.* FROM mms AS t1 INNER JOIN mms AS t2 ON t1.date = t2.date AND (t1.body = t2.body OR (t1.body IS NULL AND t2.body IS NULL)) AND t1.thread_id = t2.thread_id AND t1.address = t2.address AND t1.read = t2.read AND t1.msg_box = t2.msg_box AND t1.date_received = t2.date_received AND t1._id <> t2._id) AS doubles ORDER BY date ASC, date_received ASC, body ASC, thread_id ASC, address ASC, read ASC, msg_box ASC, _id ASC) t WHERE RNum%2 = 0)");
   std::cout << "  Removed " << d_database.changed() << " entries." << std::endl;
 

signalbackup/signalbackup.h
@@ -71,7 +71,7 @@ class SignalBackup
                                          bool overwrite = false, bool keepattachmentdatainmemory = true);
   [[nodiscard]] bool exportBackupToFile(std::string const &filename, std::string const &passphrase,
                                         bool overwrite = false, bool keepattachmentdatainmemory = true);
-  [[nodiscard]] bool exportBackupToDir(std::string const &directory, bool overwrite = false);
+  [[nodiscard]] bool exportBackupToDir(std::string const &directory, bool overwrite = false, bool keepattachmentdatainmemory = true);
   bool exportXml(std::string const &filename, bool overwrite, bool includemms = false, bool keepattachmentdatainmemory = true) const;
   void exportCsv(std::string const &filename, std::string const &table) const;
   inline void listThreads() const;
@@ -170,7 +170,7 @@ inline bool SignalBackup::exportBackup(std::string const &filename, std::string 
                                        bool keepattachmentdatainmemory)
 {
   if (bepaald::fileOrDirExists(filename) && bepaald::isDir(filename))
-    return exportBackupToDir(filename, overwrite);
+    return exportBackupToDir(filename, overwrite, keepattachmentdatainmemory);
   return exportBackupToFile(filename, passphrase, overwrite, keepattachmentdatainmemory);
 }
 

signalbackup/updaterecipientid.cc
@@ -201,6 +201,8 @@ void SignalBackup::updateRecipientId(long long int targetid, std::string ident)
 
   long long int sourceid = results.getValueAs<long long int>(0, "_id");
 
-  std::cout << "  Mapping " << sourceid << " -> " << targetid << " (" << ident << ")" << std::endl;
+  //std::cout << "  Mapping " << sourceid << " -> " << targetid << " (" << ident << ")" << std::endl;
+
+  updateRecipientId(targetid, sourceid, false);
 
 }

sqlitedb/sqlitedb.h
@@ -63,6 +63,7 @@ class SqliteDB
     template <typename T>
     inline bool contains(T const &value) const;
     bool removeColumn(uint idx);
+    inline bool removeRow(uint idx);
 
    private:
     std::wstring wideString(std::string const &narrow) const;
@@ -100,6 +101,7 @@ class SqliteDB
   inline long long int lastInsertRowid() const;
   inline bool containsTable(std::string const &tablename) const;
   inline bool tableContainsColumn(std::string const &tablename, std::string const &columnname) const;
+  inline void freeMemory();
 
  private:
   inline int execParamFiller(sqlite3_stmt *stmt, int count, std::string const &param) const;
@@ -262,6 +264,11 @@ inline bool SqliteDB::tableContainsColumn(std::string const &tablename, std::str
   return false;
 }
 
+inline void SqliteDB::freeMemory()
+{
+  sqlite3_db_release_memory(d_db);
+}
+
 inline void SqliteDB::QueryResults::emplaceHeader(std::string &&h)
 {
   d_headers.emplace_back(h);
@@ -397,6 +404,15 @@ inline uint64_t SqliteDB::QueryResults::charCount(std::string const &utf8) const
     else if ((utf8[i] & 0b11100000) == 0b11000000)
       --ret;
   return ret;
+}
+
+inline bool SqliteDB::QueryResults::removeRow(uint idx)
+{
+  if (idx >= d_values.size())
+    return false;
+
+  d_values.erase(d_values.begin() + idx);
+  return true;
 }
 
 #endif

sqlstatementframe/sqlstatementframe.h
@@ -106,6 +106,7 @@ inline SqlStatementFrame::SqlStatementFrame(unsigned char *data, size_t length, 
   :
   BackupFrame(data, length, count)
 {
+  //std::cout << "CREATING SQLSTATEMENTFRAME" << std::endl;
   for (auto const &p : d_framedata)
     if (std::get<0>(p) == FIELD::PARAMETERS)
     {
@@ -117,7 +118,6 @@ inline SqlStatementFrame::SqlStatementFrame(unsigned char *data, size_t length, 
       }
     }
 }
-
 
 inline SqlStatementFrame::SqlStatementFrame(SqlStatementFrame &&other)
   :
@@ -183,6 +183,7 @@ inline SqlStatementFrame &SqlStatementFrame::operator=(SqlStatementFrame const &
 
 inline SqlStatementFrame::~SqlStatementFrame()
 {
+  //std::cout << "DESTROYING SQLSTATEMENTFRAME" << std::endl;
   for (uint i = 0; i < d_parameterdata.size(); ++i)
     if (std::get<1>(d_parameterdata[i]))
       delete[] std::get<1>(d_parameterdata[i]);