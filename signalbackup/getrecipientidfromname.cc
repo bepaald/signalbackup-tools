6348c89c62586d81b044c2917487660d083f64ef
Selwin van Dijk <basjetimmer@yahoo.com>
2023-08-23 14:57:11 +0200

    Should finish #109

--- signalbackup/getrecipientidfromname.cc
+++ signalbackup/getrecipientidfromname.cc
@@ -19,18 +19,20 @@
 
 #include "signalbackup.ih"
 
-long long int SignalBackup::getRecipientIdFromName(std::string const &name) const
+long long int SignalBackup::getRecipientIdFromName(std::string const &name, bool withthread) const
 {
   SqliteDB::QueryResults results;
-  if (d_database.exec("SELECT recipient._id FROM recipient LEFT JOIN groups ON recipient.group_id = groups.group_id WHERE "
+
+  if (d_database.exec("SELECT recipient._id, thread._id FROM recipient LEFT JOIN groups ON recipient.group_id = groups.group_id LEFT JOIN thread ON recipient._id = thread." + d_thread_recipient_id + " WHERE "
                       "COALESCE(NULLIF(recipient." + d_recipient_system_joined_name + ", ''), " +
                       (d_database.tableContainsColumn("recipient", "profile_joined_name") ? "NULLIF(recipient.profile_joined_name, ''),"s : ""s) +
                       "NULLIF(recipient." + d_recipient_profile_given_name + ", ''), NULLIF(groups.title, ''), NULLIF(recipient." + d_recipient_aci + ", ''), NULLIF(recipient." + d_recipient_e164 + ", ''), "
-                      " recipient._id) = ?", name, &results))
+                      " recipient._id) = ?" + (withthread ? " AND thread._id IS NOT NULL" : ""), name, &results))
   {
-    // no results, or unexpected type
-    if (results.rows() == 0 ||
-        results.valueHasType<long long int>(0, "_id"))
+    //results.prettyPrint();
+
+    // no results
+    if (results.rows() == 0)
       return -1;
 
     // multiple hits for 'name'
